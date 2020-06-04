cd7324a68f71577afd7118d20bd1c2298e5fd1c0
Renz Albert Galang <renzalbertgalang@secure-fhh-journal.dev127.0.0.1>
2020-06-04 14:22:52 +0800

    Updated API key lol

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,7 +10,7 @@ import GoogleMaps
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
 
-    GMSServices.provideAPIKey("AIzaSyCdpxxYQvPxchEdufC3KNHtIdcNR3i9UJg")
+    GMSServices.provideAPIKey("YOUR-API-KEY-HERE")
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
